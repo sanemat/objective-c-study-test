8c87ff2ef15d4ec946fc691c23a1f33cbef78d05
sanemat <o.gata.ken@gmail.com>
2011-06-20 23:34:29 +0900

    add empty bowling game.

---
+++ ObjCStudyTests/GameTests.m
@@ -0,0 +1,14 @@
+//
+//  GameTests.m
+//  ObjCStudy
+//
+//  Created by sanemat on 11/06/20.
+//  Copyright 2011 na. All rights reserved.
+//
+
+#import "GameTests.h"
+
+
+@implementation GameTests
+
+@end
