8b813a025ca4012442bcc50a58fd12ab71c3b5e3
sanemat <o.gata.ken@gmail.com>
2011-06-26 22:58:48 +0900

    initial fail.

---
+++ ObjCStudyTests2/Bowling2Test.m
@@ -0,0 +1,37 @@
+//
+//  Bowling2Test.m
+//  ObjCStudy
+//
+//  Created by sanemat on 11/06/26.
+//  Copyright 2011 na. All rights reserved.
+//
+#import <GHUnitIOS/GHUnit.h>
+@interface Bowling2 : NSObject {
+}
+@end
+@implementation Bowling2
+@end
+
+@interface Bowling2Test : GHTestCase {
+   Bowling2 *game;
+}
+@end
+
+@implementation Bowling2Test
+
+-(void)setUp
+{
+   game = [[[Bowling2 alloc] init] retain];
+}
+
+-(void)tearDown
+{
+   [game release];
+}
+
+-(void)testRoll
+{
+   GHFail(@"initial fail", nil);
+}
+
+@end
