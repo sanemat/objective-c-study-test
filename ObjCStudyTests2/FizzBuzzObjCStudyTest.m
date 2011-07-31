51dafbeea0334cefa7f1a7bb208f019e5c4608b8
sanemat <o.gata.ken@gmail.com>
2011-08-01 03:06:23 +0900

    Add GHUnit usage.

--- ObjCStudyTests2/FizzBuzzObjCStudyTest.m
+++ ObjCStudyTests2/FizzBuzzObjCStudyTest.m
@@ -5,6 +5,10 @@
 //  Created by sanemat on 11/06/26.
 //  Copyright 2011 na. All rights reserved.
 //
+// GUI usage
+// Product -> Run Then Push "Run"
+// CLI usage
+// $ GHUNIT_CLI=1 xcodebuild -target ObjCStudyTests2 -sdk iphonesimulator4.3 -configuration Debug clean build
 
 #import <GHUnitIOS/GHUnit.h>
 #import "ObjCStudyAppDelegate.h"
